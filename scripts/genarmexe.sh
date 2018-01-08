173e735aaec6386e861d5d7b622ef7ef6ef18d0e
BuShra-Abubaker <bushra.ghiath@gmail.com>
2018-01-08 10:14:47 +0100

    add genarmexe

---
+++ scripts/genarmexe.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+cd "$(dirname "$0")"/.. || exit 1
+MINCAMLC=java/mincamlc
+
+
+cd "$HOME/GitHubProjects/CompilerProjectM1MOSIG"
+for test_case in tests/CodeGen/valid/*.ml
+do
+fname=$(basename "$test_case")
+extension="${fname##*.}"
+name="${fname%.*}"
+echo -n $name": "
+ocaml $test_case > "$HOME/GitHubProjects/CompilerProjectM1MOSIG/ARM/expected/"$name.txt""
+
+$MINCAMLC $test_case 2> /dev/null 1> /dev/null
+
+cd "$HOME/GitHubProjects/CompilerProjectM1MOSIG"
+#Generate .s file
+$MINCAMLC -o ARM/actual/"$name.s" "$test_case" 2> /dev/null 1> /dev/null
+
+#Generate .o file
+cd "$HOME/GitHubProjects/CompilerProjectM1MOSIG/ARM/"
+arm-none-eabi-as -o actual/"$name.o" actual/"$name.s" libmincaml.S 2> /dev/null 1> /dev/null
+
+#Generate .arm
+arm-none-eabi-ld -o actual/"$name.arm" actual/"$name.o" 2> /dev/null 1> /dev/null
+#Generate .txt
+qemu-arm actual/"$name.arm" > actual/"$name.txt"
+
+
+if diff actual/"$name.txt" expected/"$name.txt" 2> /dev/null 1> /dev/null
+then 
+	echo "match"
+else
+	echo "not match"
+fi
+
+cd "$HOME/GitHubProjects/CompilerProjectM1MOSIG"
+done
