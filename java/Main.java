2c520902bb8963607fe7d6b054ec3992c02ec671
kuhlkrein <rsebastien07@gmail.com>
2018-01-08 14:53:54 +0100

    implem of command that the teacher want: -v -o file.s file.ml

--- java/Main.java
+++ java/Main.java
@@ -45,174 +45,242 @@ public class Main {
 					);
 		}
 		
+		
 	  static List<String> options;
+	  static String fileIn = "";
+	  static String fileOut = "";
+	  static boolean topt = false;
+	  static boolean popt = false;
+	  static boolean hopt = false;
+	  static boolean oopt=  false;
+	  static boolean asmlopt = false;
+	  static boolean vopt = false;
+	  
 	
 	  static void add_option(String s) {
-		  if (s.equals("-o")||s.equals("-h")||s.equals("-v")||s.equals("-t")||s.equals("-p")||s.equals("-asml")) {
-			  options.add(s);
+		  if (s.equals("-o")){			oopt = true; options.add(s);
+		  }else if (s.equals("-h")){	hopt = true; options.add(s);
+		  }else if (s.equals("-v")){	vopt = true; options.add(s);
+		  }else if (s.equals("-t")){	topt = true; options.add(s);
+		  }else if (s.equals("-p")){	popt = true; options.add(s);
+		  }else if (s.equals("-asml")){	asmlopt = true; options.add(s);
 		  }
 	  }
+	  
+	  static boolean checkOpt(String argv[]){
+		  int nb_file = 0 ;
+		  for (int i = 0 ; i<argv.length ; i++) {
+					if (argv[i].charAt(0) != '-'){ nb_file++; }
+		  }
+		  return (nb_file <=2);	  
+	  }
+	  
+        
+	  static boolean parseCommand(String argv[]){
+	  
+		if (!checkOpt(argv)){
+			return false;
+		}
+		boolean needIn = false;
+		boolean needOut = false;
+		
+		for (int i = 0 ;i<argv.length;i++) {
+				String s = argv[i];
+				if (s.equals("-o")){		oopt = true; options.add(s); needOut = true;
+				}else if (s.equals("-h")){	hopt = true; options.add(s);
+				}else if (s.equals("-v")){	vopt = true; options.add(s); 
+				}else if (s.equals("-t")){	topt = true; options.add(s); needIn = true;
+				}else if (s.equals("-p")){	popt = true; options.add(s); needIn = true;
+				}else if (s.equals("-asml")){	asmlopt = true; options.add(s); needIn = true;
+				}else{
+					if (needIn && needOut ){
+						return false;
+					}else if (needIn){
+						fileIn = argv[i]; needIn = false;
+					}else if (needOut){
+						fileOut = argv[i]; needOut = false;
+					}else {
+						if (fileIn.equals("")){
+							fileIn = s;
+						}
+					}
+				}
+		}
+		
+		if(hopt||(topt&&popt)){
+			return false;
+		}
+		
+		if (fileIn.equals("")){
+			fileIn = argv[argv.length-1];
+		}
+		return true;
+			  
+	  }
+	  
+	  
+	
   
   static public void main(String argv[]) {    
-	  options = new LinkedList<String>();
-	  if(argv.length!=1) {
-		  /*if (argv.length != 2){ TODO;*/
-		  if (argv.length>1) {
-			 /*if (argv.length > 2){ TODO;*/
-			  for (int i = 0 ;i<argv.length;i++) {
-				 add_option(argv[i]);
-			  }
-			  if(options.contains("-t")&&options.contains("-p")) {
-				  printHelp();
-				 // System.exit(-1);
-			  }
-		  }else {
-			  for (int i = 0 ;i<argv.length;i++) {
-				  add_option(argv[i]);
-			  }
-		  }
-	  }
-	if(options.contains("-h")) {
-		printHelp();
+		options = new LinkedList<String>();
+		  
+		if(! parseCommand(argv) ) {
+			printHelp();
 		
-	}else {
-	  
-    try {  	
-    	
-      Parser p = new Parser(new Lexer(new FileReader(argv[argv.length-1])));
-	/*Parser p = new Parser(new Lexer(new FileReader(argv[argv.length-2])));TODO;*/   
-      Exp expression = (Exp) p.parse().value;      
-      //System.out.println("------ Print here ------");
-      assert (expression != null);
-      
-      
-      if(options.contains("-v")) {
-	      System.out.println("------ AST ------");
-	      expression.accept(new PrintVisitor());
-	      System.out.println();
-	      System.out.println();}
-	  if(options.contains("-p")) {}else{
-		  if(options.contains("-v")) {System.out.println("------ Type Check ------");}
-      VisitorTypeCheck t = new VisitorTypeCheck();
+		}else {
+		  
+			try {  	
+			
+			  Parser p = new Parser(new Lexer(new FileReader(fileIn)));
+			  Exp expression = (Exp) p.parse().value;      
+			  //System.out.println("------ Print here ------");
+			  assert (expression != null);
+			  
+				    
+			  if(vopt){
+				  System.out.println("------ AST ------");
+				  expression.accept(new PrintVisitor());
+				  System.out.println();System.out.println();
+			  }
+			  
+			  if(!popt){
+				  if(vopt) {
+				  	System.out.println("------ Type Check ------");
+				  
+				  }
+					  
+				  VisitorTypeCheck t = new VisitorTypeCheck();
+				  Exp expression2 = expression.accept(new Copy());
+				  expression2.accept(t);
+				  
+				  if(vopt){
+					  if(t.errorSet){
+						  System.out.println("Error from type check");
+					  }else {
+						  System.out.println("No error from type check");
+					  }
+					  System.out.println();
+				  }
+				  
+				  if(!topt) {
+					  K_Norm k = new K_Norm();
+					  expression2 = expression2.accept(k);
+					  
+					  if(vopt) {
+						  System.out.println("------ AST K-Normalization ------");
+						  expression2.accept(new PrintVisitor());
+						  System.out.println();System.out.println();
+					  }
+					  
+					  int x = k.x;
+					 
+					  if(vopt) {
+						  System.out.println("------ AST Alpha-Conversion ------");
+					  }
+					  
+					  alpha_conversion alpha = new alpha_conversion(x);
+					  expression2 = expression2.accept(alpha);
+					 
+					  if(vopt) {
+						  expression2.accept(new PrintVisitor());
+						  System.out.println();System.out.println();
+					   
+						  System.out.println("------ AST Nested-Let ------");
+					  }
+					  
+					  
+					  expression2 = expression2.accept(new Nested_Let());
+					  
+					  if(vopt) {
+						  expression2.accept(new PrintVisitor());
+						  System.out.println();System.out.println();
+					  }
+					  
+					  Closure c = new Closure();
+					  expression2.accept(c);
+					  
+					  
+					  if(vopt) {
+						System.out.println("------ AST Closure ------");
+						for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
+						  c.closure_list.get(i).print();
+						  System.out.println();
+						} System.out.println();
+					  
+					  
+						System.out.println("------ AST ASML Gen ------");
+					  }
+					  
+					  
+					  for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
+						  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new ASML_Gen(c.fun_List)));
+						  
+						  if(vopt) {
+							  c.closure_list.get(i).printASML();
+							  System.out.println();System.out.println();
+						  }
+					  }  
+					  expression2 = expression2.accept(new Reg_Alloc());
+					  
+					  String s = fileOut;
+					  
+					  File f = new File(s);
+					  FileWriter fw = new FileWriter(f) ;					  
+					  if(vopt) {
+						  System.out.println("------ AST Register Allocation ------");
+					  }
+					  
+					  for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
+						  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new Reg_Alloc()));
+						  
+						  if(vopt) { c.closure_list.get(i).printASML();}
+						  
+						  c.closure_list.get(i).printIn(fw);
+						  
+						  if(vopt){System.out.println();}
+					  }       
+					  fw.close();
+					  
+					  if(vopt) {
+					 	 System.out.println();
+					 	 
+					  }
+						
+					  if (!asmlopt){
+						  String s_arm = fileOut;
+						  
+						  File f_arm = new File(s_arm);
+						  FileWriter fw_arm = new FileWriter(f_arm) ;
+						  if (vopt) {
+							  System.out.println("------ ARM Generation ------");
+						  }
+						  for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
+							  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new ARM_Gen(fw_arm)));
+							  
+							  if(vopt){
+							 	 System.out.println();System.out.println();
+							  }
+						  }       
+						  fw_arm.close();     
+					 } 
+					/*
+					  if(vopt){
+						  System.out.println("------ Height of the initial AST ----");
+						  int height = Height.computeHeight(expression);
+						  System.out.println("using Height.computeHeight: " + height); 
 
-      Exp expression2 = expression.accept(new Copy());
-      expression2.accept(t);
-      if(options.contains("-v")) {
-	      if(t.errorSet){
-	    	  System.out.println("Error from type check");
-	      }else {
-	    	  System.out.println("No error from type check");
-	      }
-	      System.out.println();
-      }
-      if(options.contains("-t")) {}else {
-      K_Norm k = new K_Norm();
-      expression2 = expression2.accept(k);
-      
-      if(options.contains("-v")) {
-	      System.out.println("------ AST K-Normalization ------");
-	      expression2.accept(new PrintVisitor());
-	      System.out.println();
-	      System.out.println();}
-      
-      int x = k.x;
-     
-      if(options.contains("-v")) {
-    	  System.out.println("------ AST Alpha-Conversion ------");
-      }
-      alpha_conversion alpha = new alpha_conversion(x);
-      expression2 = expression2.accept(alpha);
-     
-      if(options.contains("-v")) {
-	      expression2.accept(new PrintVisitor());
-	      System.out.println();
-	      System.out.println();
-            
-	      System.out.println("------ AST Nested-Let ------");
-      }
-      expression2 = expression2.accept(new Nested_Let());
-      
-      if(options.contains("-v")) {
-	      expression2.accept(new PrintVisitor());
-	      System.out.println();System.out.println();
-      }
-      Closure c = new Closure();
-      expression2.accept(c);
-      
-      
-      if(options.contains("-v")) {
-	      System.out.println("------ AST Closure ------");
-	      for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
-	    	  c.closure_list.get(i).print();
-	          System.out.println();
-	      } System.out.println();
-      
-      
-	System.out.println("------ AST ASML Gen ------");
-      }
-      for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
-    	  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new ASML_Gen(c.fun_List)));
-    	  
-    	  if(options.contains("-v")) {
-	    	  c.closure_list.get(i).printASML();
-	          System.out.println();
-    	  }
-          System.out.println();
-      }  
-      expression2 = expression2.accept(new Reg_Alloc());
-      
-	/*TODO; add -asml flag depending on the last parameter. same for s file.*/
-      String[] tab = argv[argv.length-1].split("/", argv[argv.length-1].length());
-      
-      String s = "asml/"+ tab[tab.length-1].replace(".ml", ".asml");
-      
-      File f = new File(s);
-      FileWriter fw = new FileWriter(f) ;
-      if(options.contains("-v")) {
-    	  System.out.println("------ AST Register Allocation ------");
-      }
-      for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
-    	  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new Reg_Alloc()));
-    	  if(options.contains("-v")) { c.closure_list.get(i).printASML();}
-    	  if(options.contains("-asml")){c.closure_list.get(i).printIn(fw);}
-          System.out.println();
-      }       
-      fw.close();
-      if(options.contains("-v")) {
-      System.out.println();
-
-	      // String s_arm = "/home/khoa/Study/armExamples/"+ tab[tab.length-1].replace(".ml", ".s");
-      String s_arm = "ARM/" + tab[tab.length-1].replace(".ml", ".s");
-      File f_arm = new File(s_arm);
-      FileWriter fw_arm = new FileWriter(f_arm) ;
-
-      System.out.println("------ ARM Generation ------");
-      for (int i = c.closure_list.size()-1 ; i >=0 ; i--){
-    	  c.closure_list.get(i).set_Exp(c.closure_list.get(i).code.accept(new ARM_Gen(fw_arm)));
-          System.out.println();
-          System.out.println();
-      }       
-      fw_arm.close();      
-
-	      System.out.println("------ Height of the initial AST ----");
-	      int height = Height.computeHeight(expression);
-	      System.out.println("using Height.computeHeight: " + height); 
-	
-	      ObjVisitor<Integer> v = new HeightVisitor();
-	      height = expression.accept(v);
-	      System.out.println("using HeightVisitor: " + height);
-
-	    	System.exit(0);
-      }
-      }}
-    } catch (Exception e) {
-    	
-    	//printHelp();
-    	System.exit(1);
-    	//e.printStackTrace();
-    	
-    }
-  }
+						  ObjVisitor<Integer> v = new HeightVisitor();
+						  height = expression.accept(v);
+						  System.out.println("using HeightVisitor: " + height);
+					  }
+					 */
+				  }
+			  }
+			  System.exit(0);
+			} catch (Exception e) {
+				System.exit(1);    	
+			}
+	  }
   }
 }
 
