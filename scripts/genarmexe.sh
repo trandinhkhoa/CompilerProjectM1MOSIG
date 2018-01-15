8667fba4cc881361a6205d03cb13cec68903ded0
BuShra-Abubaker <bushra.ghiath@gmail.com>
2018-01-15 18:24:12 +0100

    add argument option to test

--- scripts/genarmexe.sh
+++ scripts/genarmexe.sh
@@ -1,49 +1,29 @@
 #! /bin/sh
 cd "$(dirname "$0")"/.. || exit 1
-sfile=$1
-wc $sfile
-
 MINCAMLC=java/mincamlc
 
-Red='\033[1;31m'
-Green='\033[1;32m'
-Yellow='\033[1;33m'
-
-Bold=$(tput bold)
-Normal=$(tput sgr0)
-Uline=$(tput smul)
-NUline=$(tput rmul)
-NC='\033[0m'
-
-echo "${Yellow}${Bold}${Uline}Code Gen Test:${NC}${Normal}${NUline}"
-
-for test_case in tests/gen-code/*.ml
-do
-fname=$(basename "$test_case")
+sfile=$1
+fname=$(basename "../CompilerProjectM1MOSIG/ARM/actual/$sfile")
 extension="${fname##*.}"
 name="${fname%.*}"
-echo -n $name": "
-ocaml $test_case > "./ARM/expected/"$name.expected"" 2> /dev/null
 
-cd "../CompilerProjectM1MOSIG"
-#Generate .s file
-$MINCAMLC -o ARM/actual/"$name.s" "$test_case" 2> /dev/null 1> /dev/null
+if [ $# -gt 0 ]; then
+    ocaml $name.ml > $name.expected 2> /dev/null
+	#Generate .o file
+	arm-none-eabi-as -o $name.o $name.s libmincaml.S 
 
-#Generate .o file
-cd "./ARM"
-arm-none-eabi-as -o actual/"$name.o" actual/"$name.s" libmincaml.S 2> /dev/null 1> /dev/null
+	#Generate .arm
+	arm-none-eabi-ld -o $name.arm $name.o 2> /dev/null 1> /dev/null
+	#Generate .actual
+	qemu-arm $name.arm > $name.actual 
 
-#Generate .arm
-arm-none-eabi-ld -o actual/"$name.arm" actual/"$name.o" 2> /dev/null 1> /dev/null
-#Generate .txt
-qemu-arm actual/"$name.arm" > actual/"$name.actual"
+	if diff $name.actual $name.expected 2> /dev/null 1> /dev/null
+	then 
+		echo "OK"
+	else
+		echo "KO"
+	fi
 
-if diff actual/"$name.actual" expected/"$name.expected" 2> /dev/null 1> /dev/null
-then 
-	echo "${Green}|-> OK${NC}"
 else
-	echo "${Red}|-> KO${NC}"
+    echo "Argument Missing: Please Enter .s file as an argument"
 fi
-cd "../"
-
-done
--- scripts/genarmexe_file.sh
+++ scripts/genarmexe_file.sh
@@ -2,22 +2,28 @@
 cd "$(dirname "$0")"/.. || exit 1
 MINCAMLC=java/mincamlc
 
+sfile=$1
+fname=$(basename "../CompilerProjectM1MOSIG/ARM/actual/$sfile")
+extension="${fname##*.}"
+name="${fname%.*}"
 
-ocaml tests/CodeGen/valid/midsub3.ml > ARM/expected/midsub4.txt
-$MINCAMLC tests/CodeGen/valid/midsub4.ml 2> /dev/null 1> /dev/null
-#Generate .s file
-$MINCAMLC -o ARM/actual/midsub4.s tests/CodeGen/valid/midsub4.ml 2> /dev/null 1> /dev/null
-#Generate .o file
-cd "../ARM/"
-arm-none-eabi-as -o actual/midsub4.o actual/midsub4.s libmincaml.S 2> /dev/null 1> /dev/null
-#Generate .arm
-arm-none-eabi-ld -o actual/midsub4.arm actual/midsub4.o 2> /dev/null 1> /dev/null
-#Generate .txt
-qemu-arm actual/midsub4.arm > actual/midsub4.txt 
+if [ $# -gt 0 ]; then
+    ocaml $name.ml > $name.expected 2> /dev/null
+	#Generate .o file
+	arm-none-eabi-as -o $name.o $name.s libmincaml.S 
 
-if diff actual/midsub4.txt expected/midsub4.txt 2> /dev/null 1> /dev/null
-then 
-	echo "match"
+	#Generate .arm
+	arm-none-eabi-ld -o $name.arm $name.o 2> /dev/null 1> /dev/null
+	#Generate .actual
+	qemu-arm $name.arm > $name.actual 
+
+	if diff $name.actual $name.expected 2> /dev/null 1> /dev/null
+	then 
+		echo "OK"
+	else
+		echo "KO"
+	fi
+
 else
-	echo "not match"
+    echo "Argument Missing: Please Enter .s file as an argument"
 fi
