7d1ed5432af96f9afe12eeacf45476df916c8f82
BuShra-Abubaker <bushra.ghiath@gmail.com>
2018-01-15 18:52:41 +0100

    update script genarmexe

--- scripts/genarmexe.sh
+++ scripts/genarmexe.sh
@@ -3,14 +3,14 @@ cd "$(dirname "$0")"/.. || exit 1
 MINCAMLC=java/mincamlc
 
 sfile=$1
-fname=$(basename "../CompilerProjectM1MOSIG/ARM/actual/$sfile")
+fname=$(basename "$sfile")
 extension="${fname##*.}"
 name="${fname%.*}"
 
 if [ $# -gt 0 ]; then
     ocaml $name.ml > $name.expected 2> /dev/null
 	#Generate .o file
-	arm-none-eabi-as -o $name.o $name.s libmincaml.S 
+	arm-none-eabi-as -o $name.o $name.s ARM/libmincaml.S 
 
 	#Generate .arm
 	arm-none-eabi-ld -o $name.arm $name.o 2> /dev/null 1> /dev/null
